778a4773a019870c94edfc6e1467b2bfdbd43a10
yeonhee-cho <yhcho.proj@gmail.com>
2025-08-26 10:23:50 +0900

    Loop for 조건문

---
+++ src/edu/control/loop/LoopRun.java
@@ -0,0 +1,16 @@
+package edu.control.loop;
+/*
+*
+* 루프 기능 실행용 클래스
+* */
+public class LoopRun {
+    // 메인 실행 메서드 작성 후
+    // 프로그램 실행 시 수행할 클래스와 메서드를 중괄호 내부에 작성
+    public static void main(String[] args) {
+        LoopEx ex =  new LoopEx();
+        // ex.method1();
+        // ex.method2();
+        // ex.method3();
+        ex.method4();
+    }
+}
