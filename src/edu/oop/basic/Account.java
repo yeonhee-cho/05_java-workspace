dd2f85c091c15b35423058b002d8e9d30f1597cd
yeonhee-cho <yhcho.proj@gmail.com>
2025-08-26 15:24:38 +0900

    Account 주석

--- src/edu/oop/basic/Account.java
+++ src/edu/oop/basic/Account.java
@@ -2,6 +2,7 @@ package edu.oop.basic;
 // basic.Account
 /*
 * 계좌 클래스
+* 추후 데이터 베이스(DB)와 자바(Java)를 연결할 변수 명칭을 작성하는 클래스
 * */
 public class Account {
     // [캡슐화]
@@ -9,5 +10,6 @@ public class Account {
     // (private : 현재 객체만 접근 가능한, 사적인 , 개인적인)
 
     // - 속성에 직접 접근을 할 수 없으므로 대신 간접 접근을 할 수 있는 기능을 작성해야 함
+
     // 속성(값)
 }
