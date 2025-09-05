fe56f91c1b1ce74694aa4708561e03580f769750
yeonhee-cho <yhcho.proj@gmail.com>
2025-09-05 17:49:01 +0900

    Exception

--- src/edu/exception/pack3/ex/model/Cart.java
+++ src/edu/exception/pack3/ex/model/Cart.java
@@ -1,4 +1,31 @@
 package edu.exception.pack3.ex.model;
 
+import java.time.LocalDateTime;
+
+/**
+ * 장바구니 모델
+ */
 public class Cart {
+    private String userId;
+    private LocalDateTime creatTime;
+
+    public Cart(String userId){
+        this.userId = userId;
+        this.creatTime = LocalDateTime.now(); // 장바구니 생성된 시간은 개발자가 설정!
+        // 현재 시간을 작성
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public LocalDateTime getCreatTime() {
+        return creatTime;
+    }
+    public void setCreatTime(LocalDateTime creatTime) {
+        this.creatTime = creatTime;
+    }
+
 }
--- src/edu/exception/pack3/ex/run/CartRun.java
+++ src/edu/exception/pack3/ex/run/CartRun.java
@@ -1,4 +1,24 @@
 package edu.exception.pack3.ex.run;
 
+import edu.exception.pack3.ex.model.Cart;
+import edu.exception.pack3.ex.service.CartService;
+
+import java.util.Scanner;
+
 public class CartRun {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("사용자 아이디를 입력하세요 : ");
+        String creatUserId = sc.nextLine();
+
+        Cart cart = new Cart(creatUserId); // userId 를 가지고 있는 cart 공간이 생성
+
+        CartService cartService = new CartService();
+        // cartService.장바구니시간체크기능(creatUserId);
+        // -> 둘 다 유저 아이디를 가져오는 것 -> html 대신 작성한 것이므로 나중에는 사용불가한 방법
+        cartService.장바구니시간체크기능(cart.getUserId());
+        // -> 나중에 html에서 가져온 데이터를 활용할 방법
+        // 지금은 creatUserId가 되지만 html을 사용하면서 데이터를 받아오는 추후에는 cart.getUserId()만 사용 가능함!
+
+    }
 }
