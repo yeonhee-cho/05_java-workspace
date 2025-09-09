e2e25480e0fd8ef469ae427982c924e4f257616c
yeonhee-cho <yhcho.proj@gmail.com>
2025-09-09 16:10:48 +0900

    IO(입출력)

--- src/edu/io/pack4/run/파일생성삭제실행.java
+++ src/edu/io/pack4/run/파일생성삭제실행.java
@@ -1,4 +1,16 @@
 package edu.io.pack4.run;
 
 public class 파일생성삭제실행 {
+    // createFolder
+    
+    // createFile
+    
+    // 각 메서드 생성 후
+    // createFolder 를 이용해서 file/store 이라는 폴더 생성
+    
+    // createFile 에서는 
+    // file/store 폴더가 존재하는지 확인하고
+    // 존재하면 Book.txt 파일 생성
+    // 가나다책방 - 책 리스트 한 줄 추가 후 
+    // 생성 완료까지 확인
 }
